3781e430d7bc33cef33bf7878b339b88ceab9321
Eduard Castany <eduard.castany@gmail.com>
2017-04-07 13:00:08 +0200

    updated example: player_remote_interface.py

--- player_remote_interface.py
+++ player_remote_interface.py
@@ -3,11 +3,10 @@
 from protopixel import Content
 
 # We neeed access to low level API calls for communication with core
-from protopixel.core import callParameterProgram
+from protopixel.core import setConfContent, callParameterContent
 
-# Define content
+# Define your content
 content = Content('Player Remote Interface Script')
-content.add_parameter('video_content', value='video')
 
 # Place where we have the custom remote interface
 content.web_path('player_remote_interface')
@@ -21,12 +20,12 @@ def websocket(ws, data):
     if action == 'play':
         # Play video
         # callParameterProgram is used to "push a button" of a particular content
-        # In this case, the button for play/pause has the name 'pause/resume' and it
-        # is in the section 'params', so the full name is 'params:pause/resume'.
-        callParameterProgram(parameters.video_content, 'params:pause/resume')
+        # In this case, the button for play has the name 'resume' and it
+        # is in the section 'params', so the full name is 'params:resume'.
+        callParameterContent("myvideo", "params:resume")
     elif action == 'pause':
         # Pause video
-        callParameterProgram(parameters.video_content, 'params:pause/resume')
+        callParameterContent("myvideo", "params:pause")
 
     # In the case that we want to modify a parameter of another content, instead of just pushing a button
     # We should use saveConfProgram from the module protopixel.core. For instance:
